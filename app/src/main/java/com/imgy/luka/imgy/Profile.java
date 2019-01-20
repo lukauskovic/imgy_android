ff0a2f1a011b9ec7df0c7364bb9f078f2afe9e61
Luka <uskovicluka@gmail.com>
2019-01-20 16:14:41 +0100

    logout option added

--- app/src/main/java/com/imgy/luka/imgy/Profile.java
+++ app/src/main/java/com/imgy/luka/imgy/Profile.java
@@ -1,24 +1,31 @@
 package com.imgy.luka.imgy;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class Profile extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class Profile extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
     private BottomNavigationView navigationView;
+    private Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         this.navigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        this.logoutButton = (Button) findViewById(R.id.logoutButton);
+        this.logoutButton.setOnClickListener(this);
         navigationView.setOnNavigationItemSelectedListener(this);
         navigationView.setSelectedItemId(R.id.navigation_profile);
     }
+
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -30,12 +37,24 @@ public class Profile extends AppCompatActivity implements BottomNavigationView.O
             } else if (itemId == R.id.navigation_upload) {
                 startActivity(new Intent(this, Upload.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
                 overridePendingTransition(0, 0);
-            }
-            else if(itemId == R.id.navigation_profile){
+            } else if (itemId == R.id.navigation_profile) {
                 return;
             }
             finish();
         }, 0);
         return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if(view == logoutButton){
+            SharedPreferences pref = getApplicationContext().getSharedPreferences("prefs", 0); // 0 - for private mode
+            SharedPreferences.Editor editor = pref.edit();
+            editor.clear();
+            editor.apply();
+            Intent signUpIntent = new Intent(this.getApplicationContext(), Login.class);
+            this.startActivity(signUpIntent);
+        }
     }
 }
